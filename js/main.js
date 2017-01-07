39e70f8da6dd53a3f818af432b76f860e99fc25d
Junior DevLeague <jrdevleague19@Juniors-iMac.local>
2017-01-07 09:36:15 -1000

    finished step 3

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 console.log(Phaser);
-//Object {VERSION: "2.6.2", GAMES: Array[0], AUTO: 0, CANVAS: 1, WEBGL: 2}
+
+//This sets the variable for the spacebar.
+var spaceKey;
+
+var ground;
+var player;
+var obstacle;
+
+//This sets the score to start at -1.
+var score = -1;
+
+
+var GAME_WIDTH = 800;
+var GAME_HEIGHT = 600;
+var GAME_CONTAINER_ID = 'gameDiv';
+
+//This is the object which runs the game.
+function preload(){
+
+},
+
+function create(){
+
+};
+
+function update(){
+
+};
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv', { preload: preload, update: update, create: create });
+
+game.state.start();
