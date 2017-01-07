b5ce59f1649c3dc621337ae9d3c9ae96b119eb11
Junior DevLeague <jrdevleague19@Juniors-iMac.local>
2017-01-07 11:28:10 -1000

    finished all steps

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var player;
 var obstacle;
 
 //This sets the score to start at -1.
-var score = -1;
+var score = 0;
 
 
 var GAME_WIDTH = 800;
@@ -25,7 +25,7 @@ function preload(){
   game.load.image('obstacle', 'assets/wallVertical.png');
 
   //If you'd like to load music files, the format would look like  game.load.audio('[name of music]', ['[location for music file]']);
-}
+};
 
 function create() {
 
@@ -65,7 +65,11 @@ function create() {
   game.physics.arcade.enable(obstacle);
   obstacle.body.immovable = true;
 
-}
+  //This adds the scoreboard on the top left side of the screen.
+  scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+};
+
+
 
 function update(){
   //This is where the game engine recognizes collision betwen the player and the ground or the obstacle.
@@ -73,9 +77,38 @@ function update(){
   game.physics.arcade.collide(player, obstacle);
 
   //This allows the player to jump only if you press the space key and the player is touching the something at the bottom.
-    if (spaceKey.isDown && player.body.touching.down) {
+  //if (spaceKey.isDown && player.body.touching.down) {
+  if (spaceKey.isDown) {
       player.body.velocity.y = -300;
-    }
+  };
+
+  //This creates a place to add sound when the obstacle reaches the player.
+  if (obstacle.x > 600) {
+    obstacle.x -= 0.2;
+  };
+  //This will update the score if the player has not been pushed off the screen, and the wall has gone off the left side.
+  //var score = 0;
+  if (obstacle.x < 1){
+    score++;
+    scoreText.text = 'score: ' + score;
+  };
+  //This will create a new wall if the old wall goes off the screen.
+  if (obstacle.x < 0) {
+    obstacle.kill();
+    obstacle = game.add.sprite(900, GAME_HEIGHT, 'obstacle');
+    obstacle.scale.setTo(1,0.2);
+    obstacle.anchor.setTo(0,1);
+    game.physics.arcade.enable(obstacle);
+    obstacle.body.immovable = true;
+  };
+
+  
+  //This will tell you "You Lose!" if the player is pushed off the left side of the screen.
+  if (player.x < 0){
+    scoreText = game.add.text(350,200, 'You Lose!', {fill: '#ff0000'});
+    obstacle.kill();
+    player.kill();
+  };
 };
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv', { preload: preload, update: update, create: create });
