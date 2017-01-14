149a87f6940d691bd5ef46d8808baa789e5ff27a
llooper21 <llooper21@punahou.edu>
2017-01-14 12:12:37 -1000

    PleaseFixMe
    Please fix the sprite and obstacle changes!!

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var spaceKey;
 var ground;
 var player;
 var obstacle;
+var walk;
 
 //This sets the score to start at -1.
 var score = 0;
@@ -19,10 +20,15 @@ var GAME_CONTAINER_ID = 'gameDiv';
 function preload(){
 
 //These four things sets the assets for the game. If you want to add music or images, there is where you would preload it.
-  game.load.image('background', 'assets/background.png');
-  game.load.image('player', 'assets/player.png');
-  game.load.image('ground', 'assets/wallHorizontal.png');
-  game.load.image('obstacle', 'assets/wallVertical.png');
+  game.load.image('background', 'assets/sky.jpg');
+  game.load.spritesheet('player', 'assets/Rocket.png', 32, 32);
+  game.load.spritesheet('player2', 'assets/Cat.png', 32, 32);
+  game.load.spritesheet('player3', 'assets/Dragon2.png', 32, 32);
+  game.load.spritesheet('rocketimg', 'assets/Rocket2.png', 32, 32);
+  game.load.image('ground', 'assets/wallHorizontal2.png');
+  game.load.image('obstacle', 'assets/wallVertical2.png');
+  game.load.image('obstacle2', 'assets/wallVertical3.png');
+  game.load.image('ground2', 'assets/wallHorizontal3.png');
 
   //If you'd like to load music files, the format would look like  game.load.audio('[name of music]', ['[location for music file]']);
 };
@@ -35,7 +41,9 @@ function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
   //game.stage.backgroundColor = '#3498db';
-  game.stage.backgroundColor = '#00BFFF';
+  //game.stage.backgroundColor = '#00BFFF';
+  //game.stage.backgroundImage = 'background';
+  game.add.tileSprite(0, 0, 1000, 600, 'background');
 
   //This sets up a group call platforms. For future functionality we can set all horizontal surfaces to this group.
   platforms = game.add.group();
@@ -51,6 +59,30 @@ function create() {
   //This creates the player character at the bottom left side of the screen.
   player = game.add.sprite(game.width/8, game.world.height*(7/8), 'player');
   game.physics.arcade.enable(player);
+  walk = player.animations.add('walk');
+  player.animations.play('walk', 10, true);
+
+  player2 = game.add.sprite(700, 0, 'player2');
+  game.physics.arcade.enable(player2);
+  walk = player2.animations.add('walk');
+  player2.animations.play('walk', 10, true);
+  player2.inputEnabled = true;
+  player2.events.onInputDown.add(listener, this);
+
+  player3 = game.add.sprite(732, 0, 'player3');
+  game.physics.arcade.enable(player3);
+  walk = player3.animations.add('walk');
+  player3.animations.play('walk', 10, true);
+  player3.inputEnabled = true;
+  player3.events.onInputDown.add(listener, this);
+
+  rocketimg = game.add.sprite(668, 0, 'rocketimg');
+  game.physics.arcade.enable(rocketimg);
+  walk = rocketimg.animations.add('walk');
+  rocketimg.animations.play('walk', 10, true);
+  rocketimg.inputEnabled = true;
+  rocketimg.events.onInputDown.add(listener, this);
+
 
   //This sets the spacebar key as the input for this game.
   spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -58,17 +90,43 @@ function create() {
   player.body.bounce.y = 0.2;
   player.body.gravity.y = 600;
 
+
   //This creates the first obstacle on the right side of the screen.
-  obstacle = game.add.sprite(700,game.world.height, 'obstacle');
+  obstacle = game.add.sprite(700,GAME_HEIGHT, 'obstacle');
   obstacle.scale.setTo(1,0.2);
   obstacle.anchor.setTo(0,1);
   game.physics.arcade.enable(obstacle);
   obstacle.body.immovable = true;
 
   //This adds the scoreboard on the top left side of the screen.
-  scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+  scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#9400D3' });
 };
 
+  function listener() {
+    //player = player2;
+    player.loadTexture('player2', 0);
+
+    player.animations.add('walk');
+
+    player.animations.play('walk', 10, true);
+
+    obstacle.loadTexture('obstacle2', 0);
+
+    ground.loadTexture('ground2', 0);
+  }
+
+  function listener2() {
+    //player = player2;
+    player.loadTexture('player3', 0);
+
+    player.animations.add('walk');
+
+    player.animations.play('walk', 10, true);
+
+    obstacle.loadTexture('obstacle3', 0);
+
+    ground.loadTexture('ground3', 0);
+  }
 
 
 function update(){
@@ -92,11 +150,27 @@ function update(){
     score++;
     scoreText.text = 'score: ' + score;
   };
+
+  // if (score > 2) {
+  //   listener();
+    // player.loadTexture('player2', 0);
+
+    // player.animations.add('walk');
+
+    // player.animations.play('walk', 10, true);
+
+    // obstacle.loadTexture('obstacle2', 0);
+
+    // ground.loadTexture('ground2', 0);
+  // };
+
+
+  var y = Math.random() * (GAME_HEIGHT - 0) + 0;
   //This will create a new wall if the old wall goes off the screen.
   if (obstacle.x < 0) {
     obstacle.kill();
-    obstacle = game.add.sprite(900, GAME_HEIGHT, 'obstacle');
-    obstacle.scale.setTo(1,0.2);
+    obstacle = game.add.sprite(900,y, 'obstacle');
+    obstacle.scale.setTo(1,0.7);
     obstacle.anchor.setTo(0,1);
     game.physics.arcade.enable(obstacle);
     obstacle.body.immovable = true;
@@ -110,6 +184,18 @@ function update(){
     player.kill();
   };
 };
+
+// function changeplayer () {
+//   player.loadTexture('player2', 0);
+
+//   player.animations.add('walk');
+
+//   player.animations.play('walk', 10, true);
+
+//   obstacle.loadTexture('obstacle2', 0);
+
+//   ground.loadTexture('ground2', 0);
+// }
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameDiv', { preload: preload, update: update, create: create });
 
